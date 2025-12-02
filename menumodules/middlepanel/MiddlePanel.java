38d9241ff5d7610c6e54b53466bebedc7a538f0d
Lucrintoko <seanaraneta0@gmail.com>
2025-12-02 15:01:40 +0800

    Middle Panel Addition
    Added Middle Panel

---
+++ menumodules/middlepanel/MiddlePanel.java
@@ -0,0 +1,49 @@
+package menumodules.middlepanel;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import menumodules.middlepanel.subpanels.*;
+
+public class MiddlePanel extends JPanel
+    {
+
+        public MiddlePanel ()
+            {
+                //Middle Panel Configurations
+                this.setLayout(new BorderLayout());
+                this.setBackground(Color.BLACK);
+                //-1 Indicates to resize based on window height
+                this.setPreferredSize(new Dimension(500, -1));
+
+                //Insantiating Top and Bottom side
+                JPanel middleTopPanel = new JPanel(new BorderLayout());
+                JPanel middleBottomPanel = new JPanel(new BorderLayout());
+
+                //To make it uniform with Button Grid Panel above it
+                middleBottomPanel.setBackground(Color.BLACK);
+
+                //Insantiating ButtonGridPanel for all Company Buttons
+                ButtonGridPanel buttonGridPanel = new ButtonGridPanel();
+                middleTopPanel.add(buttonGridPanel, BorderLayout.CENTER);
+
+                //Insantiating Bottom Sub Panel for List of Companies
+                BottomSubPanel bottomSubPanel = new BottomSubPanel();
+                middleBottomPanel.add(bottomSubPanel, BorderLayout.CENTER);
+                
+                //----------------Split Pane for Middle Bottom and Top Panel----------------
+                JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, middleTopPanel, middleBottomPanel);
+
+                //Split Pane Configurations
+                splitPane.setDividerLocation(400);
+                splitPane.setResizeWeight(0.7);
+                //Disable user ability to resize the split pane
+                splitPane.setEnabled(false);
+                splitPane.setDividerSize(2); // Set the divider to be 2 pixels thin
+                splitPane.setBackground(Color.WHITE); // Attempt to make the divider white
+
+                this.add(splitPane, BorderLayout.CENTER);
+            }
+    }
